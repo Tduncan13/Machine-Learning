51f299fc826b5a8d5ffbb8a4552bd06e90e58262
Tyler Duncan <Chico55@Tylers-MacBook-Pro.local>
2019-02-20 16:07:08 -0500

    Made a Part 1 and 2 of EZ version. Working on Vectorized Mini-Batch Gradient Descent.

--- Assignment01/Assignment01_pt2.py
+++ Assignment01/Assignment01_pt2.py
@@ -4,7 +4,6 @@ import math
 import matplotlib.pyplot as plt
 from keras.utils import to_categorical
 import random
-from numba import vectorize, float64
 
 #==========================================================
 #  Written by Tyler Duncan
@@ -12,7 +11,7 @@ from numba import vectorize, float64
 #
 #  The purpose of this program is to create a Neural 
 #  Network that can classify images of handwritten 
-#  digits using only numpy.  This is part 1 of a 5 
+#  digits using only numpy.  This is part 2 of a 5 
 #  part problem set in Dr. Pawel Wocjan's Machine 
 #  Learning course at the University of Central Florida
 #==========================================================
@@ -29,7 +28,6 @@ mnist = tf.keras.datasets.mnist
 count, rows, cols = train_images_original.shape
 
 dim = rows * cols
-
 
 # Reshape into input vectors. 
 train_images = train_images_original.reshape(count, dim, 1)
@@ -60,74 +58,48 @@ class Classifier:
     initial_bias = np.random.randint(0, 1, size=None)
 
     # Initialize Weights and Bias
-    w = initial_weight
-    b = initial_bias
+    weights = initial_weight
+    bias = initial_bias
 
     # Define the sigmoid funciton. 
-   
     def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+        return 1 / (1 + math.exp(-z))
 
     # Define sigmoid prime. 
-    @vectorize([float64(float64, float64)], target='cuda')
     def sigmoid_prime(self, z):
         return self.sigmoid(z) * (1 - self.sigmoid(z))
 
-    # Define Squared Error Loss
-    @vectorize([float64(float64, float64, float64)], target='cuda')
-    def se(self, y, a):
-        return 0.5 * ((a - y) ** 2)
-
-    # Define Squared Error Loss Prime
-    @vectorize([float64(float64, float64, float64)], target='cuda')
-    def se_prime(self, a, y):
-        return a - y
 
     # Define Binary Cross Entropy Loss.
-    @vectorize([float64(float64, float64, float64)], target='cuda')
     def bce(self, y, a):
         return (-y * np.log10(a)) - ((1 - y) * np.log10(1 - a))
 
     # Everything simplifies to (a - y)x_j for Binary Cross Entropy's derivative. 
-    @vectorize([float64(float64, float64, float64, float64)], target='cuda')
     def bce_prime(self, y, a, x):
-        return x * np.subtract(a, y)
+        return np.multiply(x, a - y)
 
-    def bce_prime_b(self, y, a):
+    def bce_prime_bias(self, y, a):
         return a - y
 
-    
     def train_model_bce(self):
-        # Set Learning rate and batch size. 
-        learning_rate = 0.2
-        batch_size = 32
-        epochs = 10
-
-        for epoch in range(epochs):
-            # Train the network. 
-            for i in range(0, count, batch_size):
-                # Shuffle Training Set. 
-                s = np.arange(self.tri.shape[0])
-                np.random.shuffle(s)
-                train_image_shuffled = self.tri[s]
-                train_label_shuffled = self.trl[s]
-                train_label_batch = train_label_shuffled[i:i+batch_size]
-                x = train_image_shuffled[i:i+batch_size].reshape((32, 784)).T
-                y = train_label_batch[:,self.number].reshape((32,1)).T
-                z = (self.w.T.dot(x) + self.b)
-                a = self.sigmoid(z)
-                print(self.b)
-                loss = self.bce(y, a) 
-                print(loss)  
-                gradient_w = (( 1 / batch_size) * np.sum(self.bce_prime(y, a, x), 1)) * learning_rate * -1
-                gradient_b = (( 1 / batch_size) * np.sum(self.bce_prime_b(y, a), 1)) * learning_rate * -1
-                self.w = self.w + gradient_w.reshape(dim, 1)
-                self.b += ((1 / batch_size) * np.sum(gradient_b))
-                # b = self.bce_prime_b(y, a)
+        learning_rate = 0.5
+        # Train the network. 
+        for i in range(0, count):
+            # Only train on the numbers specific to this classifiers specialty. 
+            x = self.tri[i]
+            z = np.dot(self.weights.T, x) + self.bias
+            a = self.sigmoid(z)
+            y = int(np.argmax(self.trl[i]) == self.number)
+            loss = self.bce(y, a)   
+            gradient_w = np.multiply((learning_rate * -1), self.bce_prime(y, a, x))
+            gradient_b = self.bce_prime_bias(y, a) * learning_rate * -1
+            self.weights = np.add(self.weights, gradient_w)
+            self.bias += gradient_b
+            # bias = self.bce_prime_bias(y, a)
 
     def make_prediction(self, ti):
         x = ti
-        z = np.dot(self.w.T, x) + self.b
+        z = np.dot(self.weights.T, x) + self.bias
         return self.sigmoid(z)
         
 # Create models.
@@ -144,31 +116,31 @@ model_nine = Classifier(train_images, train_labels, test_images, test_labels, 9)
 
 # Train Models
 model_zero.train_model_bce()
-# model_one.train_model_bce()
-# model_two.train_model_bce()
-# model_three.train_model_bce()
-# model_four.train_model_bce()
-# model_five.train_model_bce()
-# model_six.train_model_bce()
-# model_seven.train_model_bce()
-# model_eight.train_model_bce()
-# model_nine.train_model_bce()
+model_one.train_model_bce()
+model_two.train_model_bce()
+model_three.train_model_bce()
+model_four.train_model_bce()
+model_five.train_model_bce()
+model_six.train_model_bce()
+model_seven.train_model_bce()
+model_eight.train_model_bce()
+model_nine.train_model_bce()
 
-# prediciton_vector = []
-# q = 5100
-# # Test Models. 
-# for q in range(0, count):
-#     prediciton_vector.append(model_zero.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_one.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_two.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_three.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_four.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_five.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_six.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_seven.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_eight.make_prediction(test_images[q]))
-#     prediciton_vector.append(model_nine.make_prediction(test_images[q]))
+prediciton_vector = []
+q = 5100
+# Test Models. 
+for q in range(0, count):
+    prediciton_vector.append(model_zero.make_prediction(test_images[q]))
+    prediciton_vector.append(model_one.make_prediction(test_images[q]))
+    prediciton_vector.append(model_two.make_prediction(test_images[q]))
+    prediciton_vector.append(model_three.make_prediction(test_images[q]))
+    prediciton_vector.append(model_four.make_prediction(test_images[q]))
+    prediciton_vector.append(model_five.make_prediction(test_images[q]))
+    prediciton_vector.append(model_six.make_prediction(test_images[q]))
+    prediciton_vector.append(model_seven.make_prediction(test_images[q]))
+    prediciton_vector.append(model_eight.make_prediction(test_images[q]))
+    prediciton_vector.append(model_nine.make_prediction(test_images[q]))
 
-# test = np.asarray(prediciton_vector)
-# print(np.argmax(test))
-# print(np.argmax(test_labels[q]))
+test = np.asarray(prediciton_vector)
+print(np.argmax(test))
+print(np.argmax(test_labels[q]))
