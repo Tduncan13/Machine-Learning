74c1cd771f4a9c55886b32224e2ff7348656e25b
Tyler Duncan <Chico55@Tylers-MacBook-Pro.local>
2019-04-03 21:43:57 -0400

    Adding README file

--- Assignment02/Assignment02.py
+++ Assignment02/Assignment02.py
@@ -2,6 +2,6 @@ from keras.datasets import cifar10
 import matplotlib
 import numpy
 
-(train_images, train_labels), (test_images, test_labels) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
-train_images.shape
+print(x_train.shape)
